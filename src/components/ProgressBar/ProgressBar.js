e73e2d62c2b301097431dd17f9c6d58514d2e7e7
Paul Heggeseth <pheggeseth@launchdarkly.com>
2023-02-11 15:26:35 -0600

    implement ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,58 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const BORDER_RADIUS = 4;
+const styles = {
+  default: {
+    '--wrapper-border-radius': `${BORDER_RADIUS}px`,
+    '--wrapper-padding': 0,
+  },
+  large: {
+    '--progress-height': '16px',
+    '--wrapper-padding': '4px',
+    '--wrapper-border-radius': `${BORDER_RADIUS * 2}px`,
+  },
+  medium: {
+    '--progress-height': '12px',
+  },
+  small: {
+    '--progress-height': '8px',
+  },
 };
+
+const ProgressBar = ({ value, size }) => {
+  const clampedValue = Math.min(Math.max(0, value), 100);
+
+  return (
+    <Wrapper aria-valuenow={clampedValue} style={{ ...styles.default, ...styles[size] }}>
+      <ProgressWrapper>
+        <Progress style={{ width: `${clampedValue}%` }} />
+      </ProgressWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div.attrs({
+  role: 'progressbar',
+  'aria-valuemin': 0,
+  'aria-valuemax': 100,
+})`
+  width: 370px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--wrapper-border-radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--wrapper-padding);
+`;
+
+const ProgressWrapper = styled.div`
+  height: var(--progress-height);
+  border-radius: 4px;
+  overflow: clip;
+`;
+
+const Progress = styled.div`
+  height: 100%;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
