1d8a5f661aeeb477f5cb904c768ebc021eefefa5
Paul Heggeseth <pheggeseth@launchdarkly.com>
2023-02-11 20:42:15 -0600

    add updated Select based on solution

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,62 +1,65 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-  const ref = useRef();
-  const [width, setWidth] = useState(0);
-
-  useLayoutEffect(() => {
-    if (ref.current && value) {
-      setWidth(ref.current.offsetWidth);
-    }
-  }, [ref.current, value]);
 
   return (
-    <>
-      <Wrapper>
-        <StyledSelect aria-label={label} value={value} onChange={onChange} style={{ width }}>
-          {children}
-        </StyledSelect>
-        <Chevron id="chevron-down" />
-      </Wrapper>
-      <VisuallyHidden aria-hidden>
-        <StyledSelect value={value} ref={ref}>
-          <option>{displayedValue}</option>
-        </StyledSelect>
-      </VisuallyHidden>
-    </>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <VisibleSelect>
+        {displayedValue}
+        <Chevron id="chevron-down" strokeWidth={2} />
+      </VisibleSelect>
+    </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: inline-flex;
-  align-items: center;
   position: relative;
+  width: max-content;
 `;
 
-const Chevron = styled(Icon)`
+const NativeSelect = styled.select`
   position: absolute;
-  right: 5px;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
 `;
 
-const StyledSelect = styled.select`
+const VisibleSelect = styled.div`
   background-color: ${COLORS.transparentGray15};
-  padding: 12px 52px 12px 16px;
+  padding: 12px 42px 12px 16px;
   border-radius: 8px;
   border: none;
   font-size: 16px;
   color: ${COLORS.gray700};
-  appearance: none;
 
-  &:hover {
+  ${NativeSelect}:focus + & {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
+`;
+
+const Chevron = styled(Icon)`
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  pointer-events: none;
 `;
 
 export default Select;
